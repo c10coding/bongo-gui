5ba41b86a038499c450b5cd824a03ebb7affdc1e
c10coding <caleb.ja.owens@gmail.com>
2021-05-20 11:02:44 -0400

    Possibly fixed the permission issue with the vanish and show commands - v1.3.1

--- src/main/java/net/dohaw/bongogui/BongoCommand.java
+++ src/main/java/net/dohaw/bongogui/BongoCommand.java
@@ -27,35 +27,48 @@ public class BongoCommand implements CommandExecutor {
             plugin.getCompassChecker().cancel();
             plugin.loadFields();
             sender.sendMessage("You have reloaded the BongoGUI config!");
-        }
 
-        if(sender instanceof Player){
-            Player pSender = (Player) sender;
-            if(args[0].equalsIgnoreCase("show") && args.length == 1 && sender.hasPermission("bongogui.show")){
-
-                if(plugin.nonShowers.contains(pSender.getUniqueId())){
-                    BongoUtils.ensureOneActivator(pSender);
-                    plugin.nonShowers.remove(pSender.getUniqueId());
-                    sender.sendMessage("The Bongo activator will show up in your inventory!");
-                }else{
-                    sender.sendMessage("You are already showing the Bongo activator!");
-                }
-
-            }else if(args[0].equalsIgnoreCase("vanish") && args.length == 1 && sender.hasPermission("bongogui.vanish")){
-                BongoUtils.removeActivator(pSender);
-                if(!plugin.nonShowers.contains(pSender.getUniqueId())){
-                    plugin.nonShowers.add(pSender.getUniqueId());
-                    sender.sendMessage("The Bongo activator will not be in your inventory anymore!");
-                }else{
-                    sender.sendMessage("The Bongo activator is already gone!");
-                }
-            }
+            return true;
 
         }else{
-            sender.sendMessage("Only players can use this command!");
+
+            if(sender instanceof Player){
+
+                Player pSender = (Player) sender;
+                if(args[0].equalsIgnoreCase("show") && args.length == 1){
+                    if(sender.hasPermission("bongogui.show")){
+                        if(plugin.nonShowers.contains(pSender.getUniqueId())){
+                            BongoUtils.ensureOneActivator(pSender);
+                            plugin.nonShowers.remove(pSender.getUniqueId());
+                            sender.sendMessage("The Bongo activator will show up in your inventory!");
+                        }else{
+                            sender.sendMessage("You are already showing the Bongo activator!");
+                        }
+                    }else{
+                        sender.sendMessage("You do not have permission to use this command!");
+                    }
+                }else if(args[0].equalsIgnoreCase("vanish") && args.length == 1){
+                    if(sender.hasPermission("bongogui.vanish")){
+                        BongoUtils.removeActivator(pSender);
+                        if(!plugin.nonShowers.contains(pSender.getUniqueId())){
+                            plugin.nonShowers.add(pSender.getUniqueId());
+                            sender.sendMessage("The Bongo activator will not be in your inventory anymore!");
+                        }else{
+                            sender.sendMessage("The Bongo activator is already gone!");
+                        }
+                    }else{
+                        sender.sendMessage("You do not have permission to use this command!");
+                    }
+                }
+
+            }else{
+                sender.sendMessage("Only players can use this command!");
+            }
+
         }
 
         return false;
+
     }
 
 }
